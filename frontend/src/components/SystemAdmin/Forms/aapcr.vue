d1f988d634957fbc4ff81c82ebbcb725b715f8ac
Evilthing <alhambramf21@gmail.com>
2022-03-11 15:01:00 +0800

    add tab and new UI for aapcr access rights

---
+++ frontend/src/components/SystemAdmin/Forms/aapcr.vue
@@ -0,0 +1,22 @@
+<template>
+   <template>
+    <a-form-item label="Activity name" v-bind="validateInfos.name">
+      <a-input v-model:value="modelRef.name" />
+    </a-form-item>
+   </template>    
+</template>
+
+
+<script>
+import { computed, defineComponent, onMounted, ref  } from 'vue';
+
+export default defineComponent({
+    name:"AapcrTab",
+    components: {
+
+    },
+    setup() {
+        
+    },
+})
+</script>
--- frontend/src/views/system/admin/index.vue
+++ frontend/src/views/system/admin/index.vue
@@ -1,13 +1,27 @@
 <template>
- <a-card style="width: 100%">
+ <!-- <a-card style="width: 100%">
    <SystemAdmin />
-   </a-card>
+   </a-card> -->
+   <a-tabs v-model:activeKey="activeKey">
+    <a-tab-pane key="1" tab="Manager"><system-admin v-if="activeKey === '1'"/></a-tab-pane>
+    <a-tab-pane key="2" tab="Forms"><form-admin v-if="activeKey === '2'"/></a-tab-pane>
+    
+  </a-tabs>
 </template>
+
 <script>
+import { defineComponent,ref } from 'vue'
 import SystemAdmin from '@/components/SystemAdmin/Admin'
-export default {
-  components: {
+import FormAdmin from '@/views/system/forms'
+export default defineComponent({
+   components: {
     SystemAdmin,
+    FormAdmin,
   },
-}
+  setup() {
+     return {
+      activeKey: ref('1'),
+    }
+  },
+})
 </script>
